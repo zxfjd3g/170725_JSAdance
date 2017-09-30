d5e6be853b1e890be146c7bc6f5f9dab10348bd2
zxfjd3g <258147149@qq.com>
2017-09-30 09:33:10 +0800

    闭包

---
+++ work/02_函数高级/04_闭包/myModule.js
@@ -0,0 +1,19 @@
+/*
+自定义的JS模块
+ */
+function myModule() {
+  // 1. 定义数据(变量)
+  var data = 'atguigu'
+  // 2. 定义操作数据的行为(函数)
+  function doSomething() {
+    console.log('doSomething()', data)
+  }
+  function doOtherthing() {
+    console.log('doOtherthing()', data)
+  }
+  // 3. 向外暴露行为(函数/包含多个函数的对象)
+  return {
+    doSomething: doSomething,
+    doOtherthing: doOtherthing
+  }
+}
